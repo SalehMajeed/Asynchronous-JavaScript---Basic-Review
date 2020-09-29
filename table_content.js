2cbdec302086c08170c3449398e0f20de4e77162
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-29 16:50:31 -0400

    table_contents

---
+++ table_content.js
@@ -0,0 +1,13 @@
+/*
+    1. Async.
+    2. HTTP Request.
+    3. Status Code.
+    4. Callback Functions.
+    5. Json Data.
+    6. Callback Hell.
+    7. Promises.
+    8. Chaining.
+    9. Fetch.
+    10. Asyn & Await.
+    11. erros.
+*/
